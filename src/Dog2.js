5f9cd55a13d00267e2356264e82a3d38b7dce5dc
fzning <zhanningFan@126.com>
2018-04-04 01:18:01 +0800

    session 10
    fix the bug of Dog2

--- src/Dog2.js
+++ src/Dog2.js
@@ -4,7 +4,7 @@
 import React from 'react'
 
 class Dog extends React.Component {
-    constructor () {
+    constructor() {
         super()
         this.state = {
             isRunning: false,
@@ -24,18 +24,66 @@ class Dog extends React.Component {
         this.setState({
             isRunning: !this.state.isRunning
         })
-        this.setState((preState) => {
-            setTimeout(() => {
-                return {
-                    isRunning: !preState.isRunning,
-                    isBarking: !preState.isBarking
-                }
-            }, 20)
-        })
+        setTimeout(() => {
+            this.setState({
+                isBarking: false,
+                isRunning: false
+            })
+        }, 20);
     }
 
     render () {
-        return (<div onClick={() => {this.bark(); this.run();}}>dog{this.state.isRunning}{this.state.isBarking}</div>)
+        return (
+            <div>
+                <div onClick={ () => {this.bark(); this.run();} }>Dog</div> 
+                <div>{ this.state.isBarking ? 'dog is barking' : 'dog is not barking' }</div>
+                <div>{ this.state.isRunning ? 'dog is running' : 'dog is not running' }</div>
+            </div>
+        )
     }
 }
+// class Dog extends React.Component {
+//     constructor(){
+//       super()
+//       this.state = {
+//         isRunning: false,
+//         isBarking: false
+//       }
+//     }
+  
+//     // 随机数（秒数）
+//     getRandomArbitrary(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+//     }
+  
+  
+//     run(){
+//       this.setState( {
+//         isRunning: true,
+//       })
+//       setTimeout(() => this.setState({ isRunning: false }), this.getRandomArbitrary(2000,5000)) //2~5秒, 20~50毫秒眼花+_+
+//     }
+  
+//     bark(){
+//       this.setState( {
+//         isBarking: true,
+//       })
+//       setTimeout(() => this.setState({ isBarking: false }), this.getRandomArbitrary(2000,5000)) //2~5秒, 20~50毫秒眼花+_+
+//     }
+  
+//     handleDog(){
+//       this.run()
+//       this.bark()
+//     }
+  
+//     render(){
+//       return (
+//         <div>
+//           <button onClick={() => {this.run();this.bark()}}>HandleDog</button>
+//           <h5>{this.state.isRunning ? 'Dog is Running' : 'Dog is stoped'}</h5>
+//           <h5>{this.state.isBarking ? 'Dog is Barking' : 'Dog is stop barking'}</h5>
+//         </div>
+//       )
+//     }
+//   }
 export default Dog
